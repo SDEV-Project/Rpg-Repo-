e3b17aa2f17698810ad785edc62e5584d3b4081f
JLett7 <93165097+JLett7@users.noreply.github.com>
2025-04-09 17:54:29 -0400

    Add files via upload

---
+++ #SDEV Project Player Classes and Map Setup.py
@@ -0,0 +1,56 @@
+#SDEV Project Player Classes and Map Setup
+
+#Player class initializer These values are placeholders
+class player:
+    def __init__(self):
+        self.name=''
+        self.hp=50
+        self.mp=0
+        self.strength=10
+        self.endurance=10
+        self.faith=10
+        self.location='starting location'
+
+        myPlayer=player()
+
+        pass
+
+
+
+
+    #Map setup
+
+    #I'm using nested dictionaries to create a grid for the map in order to navigate in the text RPG using the words 'up,down,left ,right, north, south, east, west, etc
+
+
+#Basic layout
+# 
+#      _______
+# a1-a3|_|_|_|
+# b1-b3|_|X|_|
+# c1-c3|_|_|_|
+# d1-d3|_|_|_|
+#X=starting position
+
+#Keywords set equal to the words corresponding to their commands in the game
+DESCRIPTION ='description'
+EXAMINATION='examine'
+SOLVED/COMBATFIN=False #Set the solved state or 'combat finished' state to false
+UP='up','north'
+DOWN='down', 'south'
+RIGHT= 'right', 'east'
+LEFT= 'left', 'west'
+FINISHEDROOMS={'a1':False, 'b1':False, 'c1':False, 'd1':False, 'a2':False,'b2':False, 'c2':False, 'd2':False, 'a3':False,'b3':False, 'c3':False,'d3':False} #Catalog of completed zones for the game engine
+
+zonemap={ 'a1': 
+         {
+    DESCRIPTION:""
+    EXAMINATION='examine'
+    SOLVED/COMBATFIN=False #Set the solved state or 'combat finished' state to false
+    UP='up','north'
+    DOWN='down', 'south'
+    RIGHT= 'right', 'east'
+    LEFT= 'left', 'west'
+         }
+
+}
